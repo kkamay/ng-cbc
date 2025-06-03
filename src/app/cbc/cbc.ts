32fda1760da7d728927239ec2e18b2bded1ee5dc
KK <kivanc.kamay@gmail.com>
2025-06-03 21:53:52 +0300

    Cbc is added.

---
+++ src/app/cbc/cbc.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cbc',
+  imports: [],
+  templateUrl: './cbc.html',
+  styleUrl: './cbc.scss'
+})
+export class Cbc {
+
+}
